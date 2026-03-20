d5f8563b81da89bbd0b0ef5b59a0845f035d84d2
viktorbeznosov <viktorbeznosov@mail.ru>
2026-03-20 22:02:59 +0300

    First commit

---
+++ src/main/java/ru/job4j/social_media_api/SocialMediaApiApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.social_media_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApiApplication.class, args);
+	}
+
+}
